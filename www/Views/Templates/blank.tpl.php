70b4dbc7523273383217b5bf3408d21f63438f5d
Seb <sebastien.gaudard.1@gmail.com>
2021-06-28 17:10:45 +0200

    feat: added login/regist view + templating and stuff

---
+++ www/Views/Templates/blank.tpl.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <meta name='description' content='page description'>
+    <link rel="stylesheet" href="/public/style/boostrip.css">
+    <link rel="stylesheet" href="/public/style/style.css">
+
+    <title>Title</title>
+</head>
+<body>
+<?php include $this->view ;?>
+</body>
+</html>
--- www/Views/register.view.php
+++ www/Views/register.view.php
@@ -1,19 +1,29 @@
-<section>
-	<h2>Register</h2>
-</section>
+<main class="auth">
+    <div class="row">
+        <div class="col-14 bg-primary"></div>
+        <div class="col-10">
+            <section class="auth__section">
+                <div class="auth__section__form">
+                    <h2>S'inscrire</h2>
+                    <?php App\Core\FormBuilder::render($formRegister); ?>
+                    <ul class="auth__section__form__list">
+                        <?php if (isset($success)): ?>
+                            <li style="color: green;">
+                                <?=$success; ?>
+                            </li>
+                        <?php endif; ?>
+                        <?php if (isset($errors)): ?>
+                            <?php foreach ($errors as $error): ?>
+                                <li style="color: red;">
+                                    <?=$error; ?>
+                                </li>
+                            <?php endforeach; ?>
+                        <?php endif; ?>
+                    </ul>
+                    <a href="/login" class="auth__section__form__link">Se connecter</a>
+                </div>
+            </section>
+        </div>
+    </div>
+</main>
 
-<?php if (isset($success)): ?>
-	<li style="color: green;">
-		<?=$success; ?>
-	</li>
-<?php endif; ?>
-
-<?php if (isset($errors)): ?>
-	<?php foreach ($errors as $error): ?>
-		<li style="color: red;">
-			<?=$error; ?>
-		</li>
-	<?php endforeach; ?>
-<?php endif; ?>
-
-<?php App\Core\FormBuilder::render($formRegister); ?>
