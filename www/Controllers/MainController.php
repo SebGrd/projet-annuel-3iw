e1b3a5115ba02bdbb35cfc163fae1a538ba5799a
Seb g <sebastien.gaudard.1@gmail.com>
2021-07-28 12:28:49 +0200

    feat: added homepage featured

--- www/Controllers/MainController.php
+++ www/Controllers/MainController.php
@@ -2,11 +2,14 @@
 
 namespace App\Controllers;
 use App\Core\View;
+use App\Models\Product;
 
 class MainController {
 	public function home() {
+	    $Product = new Product();
+	    $products = $Product->findAll([], [], true);
 		$view = new View('home', 'front');
-
+		$view->assign('products', $products);
 	}
 
 	public function notFound($error = 'Erreur') {
